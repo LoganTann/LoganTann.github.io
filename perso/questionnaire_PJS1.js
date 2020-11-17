9f7551f0e7a90e1aed653cd8d016a2cca17b66c7
LoganTann <logon313@hotmail.fr>
2020-11-17 12:34:24 +0100

    maj questionnnaire

--- perso/questionnaire_PJS1.js
+++ perso/questionnaire_PJS1.js
@@ -3,7 +3,10 @@ const app = new Vue({
   data: {
     introduction_step: 0,
     button_msg: ["Bonjour Logan !", "Très bien, c'est parti !", "Continuer"],
-    player_name: "Canard-Man"
+    player_name: "Canard-Man",
+    bot_conversation: [
+      {}
+    ]
   },
   computed: {
   },
@@ -13,3 +16,12 @@ const app = new Vue({
     }
   }
 });
+const script = {};
+
+script["start"] = [
+  "bot blablabla", // speaking
+  "get text", // wainting text
+  "choice choice_id", // waiting choice
+  "modal id" // modal_opened ??
+  "jump label"
+]
